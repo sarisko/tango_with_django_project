039f5f21178fe2a2818591a6755f8fd0a89283c5
Sara Kutkova <2406155k@student.gla.ac.uk>
2020-02-08 17:30:55 +0000

    Chapter 9 exercises completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rango.models import UserProfile
 from rango.models import Category, Page
 
 
@@ -10,5 +11,6 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+admin.site.register(UserProfile)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,6 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'in@ctwe&roy*cvm2q(*7p9f#d%wogr)tj4iu*kx&r4hgu9(ahs'
+LOGIN_URL = 'rango:login'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
